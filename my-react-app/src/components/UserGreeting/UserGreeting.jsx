039a58338b6c6265ad626103cc129fceaf8cf87a
Matthew Lowe <matthewjlowe11@gmail.com>
2024-07-03 17:33:56 -0400

    Add propTypes and defaultProps

--- my-react-app/src/components/UserGreeting/UserGreeting.jsx
+++ my-react-app/src/components/UserGreeting/UserGreeting.jsx
@@ -1,4 +1,5 @@
 import './UserGreeting.css';
+import PropTypes from 'prop-types';
 
 function UserGreeting(props) {
     const welcomeMessage = <h2 className="welcome-message">
@@ -12,6 +13,16 @@ function UserGreeting(props) {
         return (
             props.isLoggedIn ? welcomeMessage : loginPrompt
         );
+}
+
+UserGreeting.propTypes = {
+    isLoggedIn: PropTypes.bool,
+    username: PropTypes.string
+}
+
+UserGreeting.defaultProps = {
+    isLoggedIn: false,
+    username: "Guest",
 }
 
 export default UserGreeting;
