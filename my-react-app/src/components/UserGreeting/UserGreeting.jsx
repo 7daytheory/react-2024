684b67a9e504079019f823afe73a6388a1fbf730
Matthew Lowe <matthewjlowe11@gmail.com>
2024-07-03 17:29:44 -0400

    Clean up ternary operator, add styles

--- my-react-app/src/components/UserGreeting/UserGreeting.jsx
+++ my-react-app/src/components/UserGreeting/UserGreeting.jsx
@@ -1,10 +1,16 @@
 import './UserGreeting.css';
 
 function UserGreeting(props) {
+    const welcomeMessage = <h2 className="welcome-message">
+        Welcome {props.username}
+        </h2>
+
+    const loginPrompt = <h2 className="login-prompt">
+        Please login to continue
+        </h2>
+
         return (
-            props.isLoggedIn ?
-                <h2>Welcome {props.username}</h2>  : 
-                <h2>Please login to continue</h2>
+            props.isLoggedIn ? welcomeMessage : loginPrompt
         );
 }
 
