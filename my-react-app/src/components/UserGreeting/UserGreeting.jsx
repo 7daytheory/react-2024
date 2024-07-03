092a80519a74df019d77af90d2e458f6d1e9c697
Matthew Lowe <matthewjlowe11@gmail.com>
2024-07-03 17:25:31 -0400

    Conditional Rendering - Use ternary operators

--- my-react-app/src/components/UserGreeting/UserGreeting.jsx
+++ my-react-app/src/components/UserGreeting/UserGreeting.jsx
@@ -1,16 +1,11 @@
 import './UserGreeting.css';
 
 function UserGreeting(props) {
-        if (props.isLoggedIn) {
-            return (
-                <h2>Welcome {props.username}</h2>
-            )
-        }
-
-        //Doesn't require else statement because return statement exits
         return (
-            <h2>Please login to continue.</h2>
-        )
+            props.isLoggedIn ?
+                <h2>Welcome {props.username}</h2>  : 
+                <h2>Please login to continue</h2>
+        );
 }
 
 export default UserGreeting;
