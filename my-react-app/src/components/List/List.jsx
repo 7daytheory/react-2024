94facb6f9057f9e050529fdcb83f56aac4cb4705
Matthew Lowe <matthewjlowe11@gmail.com>
2024-07-04 15:08:18 -0400

    Sorting object list by string/numeric values

--- my-react-app/src/components/List/List.jsx
+++ my-react-app/src/components/List/List.jsx
@@ -9,8 +9,13 @@ function List () {
         {id: 5, name: "Pineapple", calories: 55},
     ];
 
+    //fruits.sort((a,b) => a.name.localeCompare(b.name)); //Alphabetical
+    //fruits.sort((a,b) => b.name.localeCompare(a.name)); //Reverse Alphabetical
+    //fruits.sort((a,b) => a.calories - b.calories); //Numeric
+    fruits.sort((a,b) => b.calories - a.calories); //Reverse Numeric
+
     const listItems = fruits.map(fruit => <li key={fruit.id}>
-        {fruit.name}: &nbsp; <b>{fruit.caloriess}</b>
+        {fruit.name}: &nbsp; <b>{fruit.calories}</b>
         </li>);
 
     return (
