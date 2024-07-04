a226af0ad4416e424d573d58943fe9ea2d40be9e
Matthew Lowe <matthewjlowe11@gmail.com>
2024-07-04 15:00:17 -0400

    Create an array of objects and display name value

--- my-react-app/src/components/List/List.jsx
+++ my-react-app/src/components/List/List.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
 function List () {
-    const fruits = ["apple", "orange", "banana", "coconut", "pineapple"];
+    const fruits = [
+        {name: "Apple", calories: 95},
+        {name: "Orange", calories: 85},
+        {name: "Banana", calories: 155},
+        {name: "Coconut", calories: 105},
+        {name: "Pineapple", calories: 55},
+    ];
 
-    const listItems = fruits.map(fruit => <li>{fruit}</li>);
+    const listItems = fruits.map(fruit => <li>{fruit.name}</li>);
 
     return (
       <ol>
