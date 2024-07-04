b76987c412c3088eba4f658b556bf07198569f80
Matthew Lowe <matthewjlowe11@gmail.com>
2024-07-04 15:05:09 -0400

    Add a key value and display calories as well

--- my-react-app/src/components/List/List.jsx
+++ my-react-app/src/components/List/List.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 
 function List () {
     const fruits = [
-        {name: "Apple", calories: 95},
-        {name: "Orange", calories: 85},
-        {name: "Banana", calories: 155},
-        {name: "Coconut", calories: 105},
-        {name: "Pineapple", calories: 55},
+        {id: 1, name: "Apple", calories: 95},
+        {id: 2, name: "Orange", calories: 85},
+        {id: 3, name: "Banana", calories: 155},
+        {id: 4, name: "Coconut", calories: 105},
+        {id: 5, name: "Pineapple", calories: 55},
     ];
 
-    const listItems = fruits.map(fruit => <li>{fruit.name}</li>);
+    const listItems = fruits.map(fruit => <li key={fruit.id}>
+        {fruit.name}: &nbsp; <b>{fruit.caloriess}</b>
+        </li>);
 
     return (
       <ol>
