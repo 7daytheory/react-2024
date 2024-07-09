254833d53fae2df2d00b407aa4d546161feef788
Matthew Lowe <matthewjlowe11@gmail.com>
2024-07-09 19:37:35 -0400

    Add unique key to each listing

--- my-react-app/src/components/Listings.jsx
+++ my-react-app/src/components/Listings.jsx
@@ -12,7 +12,7 @@ const Listings = () => {
             </h2>
             <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
                 {jobs.map((job) => (
-                    <div className='bg-white rounded-xl shadow-md relative'>
+                    <div className='bg-white rounded-xl shadow-md relative' key={job.id}>
                         <div className='p-4'>
                             <div className='mb-6'>
                                 <div className='text-gray-600 my-2'>{job.type}</div>
