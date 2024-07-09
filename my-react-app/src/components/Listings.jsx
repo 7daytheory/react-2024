aebcf4ffece5c0b5c8950fb3b657fda05439ebc0
Matthew Lowe <matthewjlowe11@gmail.com>
2024-07-09 19:09:13 -0400

    Setup component to map job listings and display them to DOM

--- my-react-app/src/components/Listings.jsx
+++ my-react-app/src/components/Listings.jsx
@@ -4,14 +4,18 @@ import jobs from '../jobs.json';
 console.log(jobs);
 
 const Listings = () => {
-    const isHome = true;
     return (
         <section className='bg-blue-50 px-4 py-10'>
           <div className='container-xl lg:container m-auto'>
             <h2 className='text-3xl font-bold text-indigo-500 mb-6 text-center'>
-              {isHome ? 'Recent Jobs' : 'Browse Jobs'}
+                Browse Jobs
             </h2>
-          </div>
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+                {jobs.map((job) => {
+                    console.log(job);
+                })}
+            </div>
+            </div>
         </section>
       );
     };
