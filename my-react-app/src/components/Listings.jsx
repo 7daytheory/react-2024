b800496c786c92643c545de76b011d5591eb10bd
Matthew Lowe <matthewjlowe11@gmail.com>
2024-07-09 19:29:48 -0400

    Import data from json file and display them

--- my-react-app/src/components/Listings.jsx
+++ my-react-app/src/components/Listings.jsx
@@ -11,9 +11,20 @@ const Listings = () => {
                 Browse Jobs
             </h2>
             <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-                {jobs.map((job) => {
-                    console.log(job);
-                })}
+                {jobs.map((job) => (
+                    <div className='bg-white rounded-xl shadow-md relative'>
+                        <div className='p-4'>
+                            <div className='mb-6'>
+                                <div className='text-gray-600 my-2'>{job.type}</div>
+                                <h3 className='text-xl font-bold'>{job.title}</h3>
+                            </div>
+
+                            <div className='mb-5'>
+                                {job.description}
+                            </div>
+                        </div>
+                    </div>
+                ))}
             </div>
             </div>
         </section>
