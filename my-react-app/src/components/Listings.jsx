04bb9813ed33adb8263e07f1ecb7b93fab38c98c
Matthew Lowe <matthewjlowe11@gmail.com>
2024-07-09 19:47:23 -0400

    Add link to single job page for Read More

--- my-react-app/src/components/Listings.jsx
+++ my-react-app/src/components/Listings.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import jobs from '../jobs.json';
-
-console.log(jobs);
+import Listing from './Listing';
 
 const Listings = () => {
     return (
@@ -12,29 +11,7 @@ const Listings = () => {
             </h2>
             <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
                 {jobs.map((job) => (
-                    <div className='bg-white rounded-xl shadow-md relative' key={job.id}>
-                        <div className='p-4'>
-                            <div className='mb-6'>
-                                <div className='text-gray-600 my-2'>{job.type}</div>
-                                <h3 className='text-xl font-bold'>{job.title}</h3>
-                            </div>
-                            <div className='mb-5'>
-                                {job.description}
-                            </div>
-                            <h3 className='text-indigo-500 mb-2'>{job.salary}</h3>
-                            <div className='border border-gray-100 mb-5'></div>
-                            <div className='flex flex-col lg:flex-row justify-between mb-4'>
-
-                            </div>
-                            <div className='text-orange-700 mb-3'>
-                                <i className='fa-solid fa-location-dot text-lg'></i>
-                                {job.location}
-                            </div>
-                            <a href='#' className='h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm'>
-                                Read More
-                            </a>
-                        </div>
-                    </div>
+                    <Listing job ={job} key={job.id} />
                 ))}
             </div>
             </div>
