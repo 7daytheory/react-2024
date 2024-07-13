e835e3a94c2d9de46f44e61780374e6db6a50ea9
Matthew Lowe <matthewjlowe11@gmail.com>
2024-07-13 15:18:32 -0400

    Change remaining a hrefs to Link to

--- my-react-app/src/components/Listing.jsx
+++ my-react-app/src/components/Listing.jsx
@@ -29,9 +29,9 @@ const Listing = ( {job}) => {
                 <FaMapMarker className="inline text-lg mb-1 mr-1" />
                 {job.location}
             </div>
-            <a href={`/job/${job.id}`} className='h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm'>
+            <Link to={`/job/${job.id}`} className='h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm'>
                 Read More
-            </a>
+            </Link>
         </div>
     </div>
   )
