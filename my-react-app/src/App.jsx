2c43516acd4289cbf40b4cc9e7a1882059768ad9
Matthew Lowe <matthewjlowe11@gmail.com>
2024-07-12 20:51:18 -0400

    Import react-router-dom elements and display content in router var

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,20 +1,26 @@
+import { Route, 
+        createBrowserRouter, 
+        createRoutesFromElements,
+        RouterProvider } 
+   from 'react-router-dom';
 import Banner from "./components/Banner";
 import HomeCards from "./components/HomeCards";
 import Listings from "./components/Listings";
 import Navbar from "./components/Navbar";
 import ViewAll from "./components/ViewAll";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route index element={<h1>My Page</h1>} />)
+)
+
 
 function App() {
-  return (
-    <>
-      <Navbar />
+  return <RouterProvider router={router} />;
+      {/* <Navbar />
       <Banner />
       <HomeCards />
       <Listings />
-      <ViewAll />
-    </>
-  );
+      <ViewAll /> */}
 }
 
 export default App
