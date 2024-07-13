ce419b95b79c65cc7a6ab3a685f5d5d6fa54033b
Matthew Lowe <matthewjlowe11@gmail.com>
2024-07-12 21:22:35 -0400

    Create jobs page and call in with assigned path inside Route

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,15 +1,17 @@
-import { Route, 
+  import { Route, 
         createBrowserRouter, 
         createRoutesFromElements,
         RouterProvider } 
-   from 'react-router-dom';
-   import HomePage from './pages/HomePage';
-   import MainLayout from './layouts/MainLayout';
+  from 'react-router-dom';
+  import HomePage from './pages/HomePage';
+  import MainLayout from './layouts/MainLayout';
+  import JobsPage from './pages/JobsPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
   <Route path='/' element={<MainLayout />}>
-    <Route index element={<HomePage />} /> //if path set to (path="/about") it will display on about page
+    <Route index element={<HomePage />} />
+    <Route path='/jobs' element={<JobsPage />} />
   </Route>
   )
 )
