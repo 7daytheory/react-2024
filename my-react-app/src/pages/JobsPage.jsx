595f5c694c79fb51e93b4ab0a46e77236e7aeb6a
Matthew Lowe <matthewjlowe11@gmail.com>
2024-07-15 10:52:19 -0400

    Pass props to pages and display data accordingly

--- my-react-app/src/pages/JobsPage.jsx
+++ my-react-app/src/pages/JobsPage.jsx
@@ -3,7 +3,7 @@ import Listings from '../components/Listings';
 const JobsPage = () => {
     return (
       <section className='bg-blue-50 px-4 py-6'>
-        <Listings />
+        <Listings isHome={false} />
       </section>
     )
   };
