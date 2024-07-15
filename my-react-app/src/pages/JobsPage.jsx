88324b5f90f55c68d2b617d4c9baa420abdf3037
Matthew Lowe <matthewjlowe11@gmail.com>
2024-07-15 10:45:19 -0400

    Display Jobs on Show all jobs page

--- my-react-app/src/pages/JobsPage.jsx
+++ my-react-app/src/pages/JobsPage.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import Listings from '../components/Listings'
+import Listings from '../components/Listings';
 
 const JobsPage = () => {
-  return (
-    <Listings />
-  )
-}
+    return (
+      <section className='bg-blue-50 px-4 py-6'>
+        <Listings />
+      </section>
+    )
+  };
 
 export default JobsPage
