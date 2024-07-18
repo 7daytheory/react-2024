b641832e33f3b6cf4fc3334bfb771e7d44f604d7
Matthew Lowe <matthewjlowe11@gmail.com>
2024-07-18 17:39:10 -0400

    Create JobPage that passes in correct job ID to job page url

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,14 @@
   import MainLayout from './layouts/MainLayout';
   import JobsPage from './pages/JobsPage';
   import NotFound from './pages/NotFound';
+  import JobPage from './pages/JobPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
   <Route path='/' element={<MainLayout />}>
     <Route index element={<HomePage />} />
     <Route path='/jobs' element={<JobsPage />} />
+    <Route path='/jobs/:id' element={<JobPage />} />
     <Route path='*' element={<NotFound />} />
   </Route>
   )
