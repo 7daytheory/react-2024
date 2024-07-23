5254a415fc42eb43537b60d357c3b95827b57fc8
Matthew Lowe <matthewjlowe11@gmail.com>
2024-07-22 20:27:20 -0400

    Cleaned up code

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -11,9 +11,8 @@ const JobPage = () => {
       <div className="container m-auto py-6 px-6">
         <Link
           to='/jobs'
-          className="text-indigo-500 hover:text-indigo-600 flex items-center"
-        >
-          <i className="fas fa-arrow-left mr-2"></i> Back to Job Listings
+          className="text-indigo-500 hover:text-indigo-600 flex items-center">
+          <i className="fas fa-arrow-left mr-2"></i>Back to Job Listings
         </Link>
       </div>
     </section>
@@ -22,19 +21,13 @@ const JobPage = () => {
       <div className="container m-auto py-10 px-6">
         <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
           <main>
-            <div
-              className="bg-white p-6 rounded-lg shadow-md text-center md:text-left"
-            >
+            <div className="bg-white p-6 rounded-lg shadow-md text-center md:text-left">
               <div className="text-gray-500 mb-4">{job.type}</div>
               <h1 className="text-3xl font-bold mb-4">
                 {job.title}
               </h1>
-              <div
-                className="text-gray-500 mb-4 flex align-middle justify-center md:justify-start"
-              >
-                <i
-                  className="fa-solid fa-location-dot text-lg text-orange-700 mr-2"
-                ></i>
+              <div className="text-gray-500 mb-4 flex align-middle justify-center md:justify-start">
+                <i className="fa-solid fa-location-dot text-lg text-orange-700 mr-2"></i>
                 <p className="text-orange-700">{job.location}</p>
               </div>
             </div>
@@ -80,8 +73,7 @@ const JobPage = () => {
               <h3 className="text-xl font-bold mb-6">Manage Job</h3>
               <Link
                 to={`jobs/edit/${job.id}`}
-                className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
-                >Edit Job</Link>
+                className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block" >Edit Job</Link>
               <button
                 onClick={() => {/*Delete job id */}}
                 className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
