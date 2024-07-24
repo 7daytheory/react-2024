b72db0ac51404c29d84c35e9918dabf8b568d642
Matthew Lowe <matthewjlowe11@gmail.com>
2024-07-24 09:40:30 -0400

    Use DELETE method to delete item from api

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,10 @@ function App() {
 
 //Delete Job
 const deleteJob = async (id) => {
-  console.log('delete', id);
+  const res = await fetch(`/api/jobs/${id}`, {
+    method: 'DELETE',
+  })
+  return;
 }
 
 const router = createBrowserRouter(
