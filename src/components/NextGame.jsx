2c59b07e0de71048e2ac876605155c9ba504fa7c
Matthew Lowe <matthewjlowe11@gmail.com>
2024-07-30 14:07:20 -0400

    Create NextGame component that will display the closest upcoming game

---
+++ src/components/NextGame.jsx
@@ -0,0 +1,13 @@
+const NextGame = () => {
+  return (
+    <div className="flex justify-center w-450px">
+    <div className="game-card bg-white p-6 rounded-lg shadow-lg border border-gray-300 text-center">
+        <h2 className="text-2xl font-bold mb-4">Next Kansas City Game</h2>
+        <p className="text-lg mb-2">vs Opponent Team</p>
+        <p className="text-gray-700">Date and Time</p>
+    </div>
+    </div>
+  )
+}
+
+export default NextGame
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import NextGame from './components/NextGame.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <NextGame />
   </React.StrictMode>,
 )
